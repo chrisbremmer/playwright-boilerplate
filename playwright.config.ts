94328e821d5877fa683705de3c3d3a560673026a
chrisbremmer <chris.bremmer@vuori.com>
2024-01-31 16:21:24 -0800

    updated workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 4 : undefined,
+    workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -74,12 +74,12 @@ export default defineConfig({
         //     channel: 'msedge',
         //   },
         // },
-        {
-            name: 'Google Chrome',
-            use: {
-                channel: 'chrome',
-            },
-        },
+        // {
+        //     name: 'Google Chrome',
+        //     use: {
+        //         channel: 'chrome',
+        //     },
+        // },
     ],
 
     /* Run your local dev server before starting the tests */
