2c9bdd617950058ed3ca5f88700273b775acf6d7
chrisbremmer <chris.bremmer@vuori.com>
2024-02-01 12:38:07 -0800

    Removed some load tests

---
+++ tests/load/scripts/flows.js
@@ -0,0 +1,11 @@
+module.exports = { helloFlow };
+
+async function helloFlow(page) {
+    //
+    // The code below is just a standard Playwright script:
+    //
+    // Go to https://artillery.io/
+    await page.goto('https://www.artillery.io/');
+    // Click text=Pricing
+    await page.click('text=Cloud');
+}
